33cd878c9578f120fce4e9efdf774bb497647f59
Cesar Miguel Villegas Flores <142122862+Cesar7771206@users.noreply.github.com>
2025-10-24 11:58:57 -0500

    Agregué contenido prueba

--- src/main/java/com/microemprendimiento/MainApp.java
+++ src/main/java/com/microemprendimiento/MainApp.java
@@ -1,7 +1,18 @@
 package com.microemprendimiento;
+import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Cesar");
+        System.out.println("Ingrese su edad");
+        System.out.print("Edad: ");
+        int edad = scanner.nextInt();
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
+        scanner.close();
     }
 }
